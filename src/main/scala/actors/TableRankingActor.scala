6262a2bb92c0ac4d54692c7955d18fb3387654db
Ivan Obeso <ivan.obeso1@gmail.com>
2017-09-03 19:14:25 +0100

    refactor

--- src/main/scala/actors/TableRankingActor.scala
+++ src/main/scala/actors/TableRankingActor.scala
@@ -32,28 +32,8 @@ class TableRankingActor extends Actor {
     }
     case HttpResponse(StatusCodes.OK, headers, entity, _) => {
       entity.dataBytes.runFold(ByteString(""))(_ ++ _).foreach { body =>
-        //val teams: List[Team] = JsonFootballParser.parseLeagueCall(body.utf8String)
         val source = body.utf8String
-        val jsonAst: JsValue = source.parseJson
-
-
-        val a: JsValue = (jsonAst.asJsObject).fields.get("data").get
-        val b = a.asInstanceOf[JsArray].elements(0)
-        val c = b.asJsObject.fields("standings")
-        val d = c.asJsObject.fields.get("data").get.asInstanceOf[JsArray].elements
-
-        val e = d(0).asJsObject.fields
-        val teams = d.map((x:JsValue) => {
-          val stats = x.asJsObject.fields
-          val points = stats("points").convertTo[Int]
-          val goalsScored = stats("home_goals_attempted").convertTo[Int]
-          val goalsConceded = stats("home_goals_scored").convertTo[Int]
-          val id = stats("team").asJsObject.fields("id").convertTo[Int]
-          val name = stats("team").asJsObject.fields("name").convertTo[String]
-          Team(id,name,points,goalsScored,goalsConceded)
-        })
-
-        System.out.println(body.utf8String)
+        val teams: List[Team] = JsonFootballParser.parseLeagueCall(source)
 
         val teamActor = system.actorOf(Props[TeamActor], "team-actor")
         teamActor ! GetInfoTeam
--- src/main/scala/services/JsonFootballParser.scala
+++ src/main/scala/services/JsonFootballParser.scala
@@ -1,11 +1,27 @@
 package services
 
 import beans.Team
+import spray.json.{JsArray, JsValue}
 
 class JsonFootballParser
 
 object JsonFootballParser{
-  def parseLeagueCall(resp: String): List[Team] = {
-    return Nil
+  def parseLeagueCall(json: String): List[Team] = {
+    val jsonAst: JsValue = json.parseJson
+
+    val a: JsValue = (jsonAst.asJsObject).fields.get("data").get
+    val b = a.asInstanceOf[JsArray].elements(0)
+    val c = b.asJsObject.fields("standings")
+    val d = c.asJsObject.fields.get("data").get.asInstanceOf[JsArray].elements
+
+    d.map((x:JsValue) => {
+      val stats = x.asJsObject.fields
+      val points = stats("points").convertTo[Int]
+      val goalsScored = stats("home_goals_attempted").convertTo[Int]
+      val goalsConceded = stats("home_goals_scored").convertTo[Int]
+      val id = stats("team").asJsObject.fields("id").convertTo[Int]
+      val name = stats("team").asJsObject.fields("name").convertTo[String]
+      Team(id,name,points,goalsScored,goalsConceded)
+    })
   }
 }
